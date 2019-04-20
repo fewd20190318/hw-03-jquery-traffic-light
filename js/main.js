59ae78ce9c885b4be54389d946fff7a9f1ad4b53
jngong <jngong@gmail.com>
2019-04-19 20:20:05 -0400

    indented js file

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 $(function(){
 
-// When any button is clicked, change the background color of all bulbs to black.
-  function clearLights(){
-    $(".bulb").css("backgroundColor", "black");
-  }
+  // When any button is clicked, change the background color of all bulbs to black.
+    function clearLights(){
+      $(".bulb").css("backgroundColor", "black");
+    }
 
-  $(".button").click(clearLights);
+    $(".button").click(clearLights);
 
-// When stopButton is clicked, change the background color of the stopLight to red.
-  function illuminateRed(){
-    $("#stopLight").css("backgroundColor", "red");
-  }
+  // When stopButton is clicked, change the background color of the stopLight to red.
+    function illuminateRed(){
+      $("#stopLight").css("backgroundColor", "red");
+    }
 
-  $("#stopButton").click(illuminateRed);
+    $("#stopButton").click(illuminateRed);
 
-// When slowButton is clicked, change the background color of the slowLight to yellow.
-  function illuminateYellow(){
-    $("#slowLight").css("backgroundColor", "yellow");
-  }
+  // When slowButton is clicked, change the background color of the slowLight to yellow.
+    function illuminateYellow(){
+      $("#slowLight").css("backgroundColor", "yellow");
+    }
 
-  $("#slowButton").click(illuminateYellow);
+    $("#slowButton").click(illuminateYellow);
 
-// When goButton is clicked, change the background color of the goLight to green.
-  function illuminateGreen(){
-    $("#goLight").css("backgroundColor", "green");
-  }
+  // When goButton is clicked, change the background color of the goLight to green.
+    function illuminateGreen(){
+      $("#goLight").css("backgroundColor", "green");
+    }
 
   $("#goButton").click(illuminateGreen);
 
