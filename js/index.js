f5a1137b2eebc82cdd57ec0cdfd7ad401d820a38
Karen <k.ortiz026@gmail.com>
2019-04-23 17:17:49 -0400

    Added function to clear lights on stop light

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 $(function() {
     console.log("ready!");
 
-    //Listen for a click event for each bulb
+    //Listen for a click event for each button
     $("#stopButton").click(illuminateRed);
     $("#slowButton").click(illuminateYellow);
     $("#goButton").click(illuminateGreen);
 
     //Functions that turn on bulbs for the Stop Light
     function illuminateRed() {
+      clearLights();
       $("#stopLight").css("background-color","red");
     }
 
     function illuminateYellow() {
+      clearLights();
       $("#slowLight").css("background-color","yellow");
     }
 
     function illuminateGreen() {
+      clearLights();
       $("#goLight").css("background-color","green");
     }
 
+    //Function to turn off all bulbs for the Stop Light
+    function clearLights() {
+      $("#stopLight").css("background-color","black");
+      $("#slowLight").css("background-color","black");
+      $("#goLight").css("background-color","black");
+    }
 });
