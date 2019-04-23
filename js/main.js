25bc45b93d513e9713627b9093663d108f9cd5df
Anthony Panayiotou <anthonypanayiotou@ool-ad020196.dyn.optonline.net>
2019-04-23 18:16:11 -0400

    began to change to jquery

--- js/main.js
+++ js/main.js
@@ -4,4 +4,25 @@ $('goButton').click(illuminateGreen);
 
 
 
-$('')
+function illuminateRed() {
+  clearLights();
+  $('stopLight').style.backgroundColor = "red";
+}
+
+// function illuminateYellow() {
+//   clearLights();
+//   document.getElementById('slowLight').style.backgroundColor = "yellow";
+// }
+//
+// function illuminateGreen() {
+//   clearLights();
+//   document.getElementById('goLight').style.backgroundColor = "green";
+// }
+
+
+function clearLights(){
+  $('stopLight').stlye.backgroundColor ="black";
+
+
+
+}
