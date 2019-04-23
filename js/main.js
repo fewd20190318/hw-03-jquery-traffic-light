a5c8d5d67c541a2c42c6819941e026bdb108ed25
Anthony Panayiotou <anthonypanayiotou@ool-ad020196.dyn.optonline.net>
2019-04-23 19:23:39 -0400

    converted whole js to jq

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+
+
 $("#stopButton").click(illuminateRed);
 $("#slowButton").click(illuminateYellow);
 $("#goButton").click(illuminateGreen);
@@ -5,16 +8,25 @@ $("#goButton").click(illuminateGreen);
 
 
 function illuminateRed() {
-
+  clearLights();
   $("body").attr("class", "red");
 }
 
 function illuminateYellow() {
-
+  clearLights()
   $("body").attr("class", "yellow");
 }
 
 function illuminateGreen() {
-
+  clearLights();
   $("body").attr("class", "green");
 }
+
+
+function clearLights() {
+  $('body').attr("class", "black");
+  $('body').attr("class", "black");
+  $('body').attr("class", "black");
+}
+
+});
