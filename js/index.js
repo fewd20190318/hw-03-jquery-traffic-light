25f222fcf39b3fa7b7c38da882fb343375fe8f31
Karen <k.ortiz026@gmail.com>
2019-04-23 17:10:21 -0400

    Slow and Go buttons now work to change the respecitve bulbs to the right color.

--- js/index.js
+++ js/index.js
@@ -3,9 +3,20 @@ $(function() {
 
     //Listen for a click event for each bulb
     $("#stopButton").click(illuminateRed);
+    $("#slowButton").click(illuminateYellow);
+    $("#goButton").click(illuminateGreen);
 
-    //Functions that turn on bulbs for the stopLight
+    //Functions that turn on bulbs for the Stop Light
     function illuminateRed() {
       $("#stopLight").css("background-color","red");
     }
+
+    function illuminateYellow() {
+      $("#slowLight").css("background-color","yellow");
+    }
+
+    function illuminateGreen() {
+      $("#goLight").css("background-color","green");
+    }
+
 });
