dd494b35f9406501cea97f27d8bd4ddacc0c506b
Lauralyn <lswatson69@gmail.com>
2019-04-24 23:34:19 -0400

    Update app.js
    add clearLight function

--- js/app.js
+++ js/app.js
@@ -6,20 +6,29 @@ $(document).ready(function(){
 
   function illuminateRed(){
     console.log("calling function")
+    clearLights();
     $('#stopLight').css('background-color', 'red');
+
     // $('#slowLight').css('background-color', 'black');
     // $('#goLight').css('background-color', 'black');
   }
 
   function illuminateYellow(){
     console.log("calling function")
+    clearLights();
     $('#slowLight').css('background-color', 'yellow');
   }
 
   function illuminateGreen(){
     console.log("calling function")
+    clearLights();
     $('#goLight').css('background-color', 'green');
   }
 
+  function clearLights(){
+    $('#stopLight').css('background-color', 'black');
+    $('#slowLight').css('background-color', 'black');
+    $('#goLight').css('background-color', 'black');
+  }
 
 });
