d44101a91c956b181a22172b3dbe542a63bda524
Allison <allison.gollub@gmail.com>
2019-04-27 09:06:24 -0400

    sheets linked correctly

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+
+/*
+  $('#grayButton').click(switchGray);
+  $('#whiteButton').click(switchWhite);
+  $('#pinkButton').click(switchPink);
+  $('#blueButton').click(switchBlue;
+
+  function switchGray() {
+    // The following code reads as the following:
+    // "Select the body tag and add a 'class' attribute with a value of 'gray'"
+    $('body').attr('class', 'gray');
+  }
+
+  function switchWhite(){
+    $('body').attr('class', 'white');
+  }
+
+  function switchPink(){
+    $('body').attr('class', 'pink');
+  }
+
+    function switchBlue(){
+      $('body').attr('class', 'blue');
+    }
+  }
+
+  */
+  // 1) Add the code to accomodate using jQuery
+  //    to make the other buttons work as expected
+
+  // 2) Add two additional buttons
+
+  // Extra Credit
+  // - Add a link labeled "Show / Hide Buttons"
+  // - When users click on the link the following should happen
+  // -- if the buttons are showing, then pressing the link should Hide them
+  // -- if the buttons are not showing, then pressing the link should Show them
+  // -- Hint: Google is your friend
+});
