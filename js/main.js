677986aae4918e371308fe6c34e4551de8b59133
Allison <allison.gollub@gmail.com>
2019-04-27 17:37:48 -0400

    green light working :)

--- js/main.js
+++ js/main.js
@@ -1,41 +1,11 @@
-$(document).ready(function(){
+$(function(){
+
+  $('#goButton').click(illuminatedGreen);
 
 
-/*
-  $('#grayButton').click(switchGray);
-  $('#whiteButton').click(switchWhite);
-  $('#pinkButton').click(switchPink);
-  $('#blueButton').click(switchBlue;
+  function illuminatedGreen(){
+    console.log("calling switch light function");
 
-  function switchGray() {
-    // The following code reads as the following:
-    // "Select the body tag and add a 'class' attribute with a value of 'gray'"
-    $('body').attr('class', 'gray');
+    $("#goLight").toggleClass("green");
   }
-
-  function switchWhite(){
-    $('body').attr('class', 'white');
-  }
-
-  function switchPink(){
-    $('body').attr('class', 'pink');
-  }
-
-    function switchBlue(){
-      $('body').attr('class', 'blue');
-    }
-  }
-
-  */
-  // 1) Add the code to accomodate using jQuery
-  //    to make the other buttons work as expected
-
-  // 2) Add two additional buttons
-
-  // Extra Credit
-  // - Add a link labeled "Show / Hide Buttons"
-  // - When users click on the link the following should happen
-  // -- if the buttons are showing, then pressing the link should Hide them
-  // -- if the buttons are not showing, then pressing the link should Show them
-  // -- Hint: Google is your friend
 });
